1516b586bd7347683ba016879467fe1e00a354d6
anhauxiang <1342181530@qq.com>
2018-09-29 22:57:04 +0800

    fix: article click count auto add

--- blog/views.py
+++ blog/views.py
@@ -117,6 +117,8 @@ def article(request):
         try:
             # 获取文章信息
             article = Article.objects.get(pk=id)
+            article.click_count += 1
+            article.save()
         # 注意捕获文章不存在的异常
         except Article.DoesNotExist:
             return render(request, 'failure.html', {'reason': '没有找到对应的文章'})
